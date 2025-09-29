68f44a86e09df64a0f1be8b9b1f47e262ed83d44
Daniele <12999345@itisrossi.vi.it>
2025-09-29 13:05:59 +0200

    Program finished

--- main.c
+++ main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+    char stringa[]="i topi non avevano nipoti";
+    char *p1= stringa;
+    char *p2= stringa;
+
+    //con il secondo puntatore arriva alla fine della stringa
+    while (*p2 != '\0')
+    p2++;
+
+    p2--;
+    //printf("%s\n", p1);
+    //printf("%s\n", p2);
+
+    while (p1<p2)
+    {
+        if (*p1==' ')
+            p1++;
+        if (*p2==' ')
+            p2--;
+        if (*p1!=*p2)
+        {
+            printf("NON palindrome");
+            return 0;
+        }
+        p1++;
+        p2--;
+
+    }
+        printf("palindrome");
     return 0;
 }
